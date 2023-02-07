0b9a8ca329a55b7c51bcaa3450144eb13f96dadd
ngab0 <jackdonnily@gmail.com>
2023-02-07 04:40:37 -0500

    4 fix

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,18 @@
-export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students
-    .filter((student) => student.location === city)
-    .map((student) => {
-      const { grade } = newGrades
-        .filter((grade) => grade.studentId === student.id).pop() || { grade: 'N/A' };
-      return { ...student, grade };
-    })
+function updateStudentGradeByCity(students, city, newGrades) {
+  const studentsCity = students.filter((student) => student.location === city);
+  return studentsCity.map((student) => {
+    const filteredGrades = newGrades.filter((grade) => grade.studentId === student.id);
+    if (filteredGrades.length > 0) {
+      return {
+        ...student,
+        grade: filteredGrades[0].grade,
+      };
+    }
+    return {
+      ...student,
+      grade: 'N/A',
+    };
+  });
 }
+
+export default updateStudentGradeByCity;
